17c7318c964b5349d0b11435baf0e95c86e82bbc
Michael Bayne <mdb@samskivert.com>
2011-08-04 10:45:04 -0700

    (Re)wrote main screen using TriplePlay UI.

--- src/main/java/atlantis/client/MainMenuScreen.java
+++ src/main/java/atlantis/client/MainMenuScreen.java
@@ -7,9 +7,21 @@ package atlantis.client;
 import java.util.Arrays;
 import java.util.HashSet;
 
-import tripleplay.game.Screen;
+import forplay.core.Font;
+import forplay.core.ForPlay;
 
-import atlantis.client.util.TextGlyph;
+import react.UnitSlot;
+
+import tripleplay.game.Screen;
+import tripleplay.ui.AxisLayout;
+import tripleplay.ui.Background;
+import tripleplay.ui.Button;
+import tripleplay.ui.Group;
+import tripleplay.ui.Label;
+import tripleplay.ui.Root;
+import tripleplay.ui.Style;
+import tripleplay.ui.Styles;
+
 import atlantis.shared.GameObject;
 import atlantis.shared.Log;
 
@@ -21,30 +33,39 @@ public class MainMenuScreen extends AtlantisScreen
     @Override // from Screen
     public void wasAdded () {
         super.wasAdded();
-        TextGlyph temp = TextGlyph.forText("TEMP!", UI.uiFormat(0xFF000000));
-        temp.layer.setTranslation(100, 100);
-        layer.add(temp.layer);
-        input.register(temp.layer, temp.bounds(), new Input.Action() {
-            public void onTrigger () {
+
+        Root root = iface.createRoot(AxisLayout.vertical().gap(25), UI.stylesheet);
+        root.addStyles(Style.BACKGROUND.is(Background.solid(0xFFFFFFFF, 5)));
+        layer.add(root.layer);
+
+        Group botrow = new Group(AxisLayout.horizontal().alignTop().gap(50));
+        Group buttons = new Group(AxisLayout.vertical().offStretch());
+        Group games = new Group(AxisLayout.vertical().offStretch());
+        botrow.add(buttons, games);
+
+        Label logo = new Label().setText("Atlantis");
+        logo.addStyles(Style.FONT.is(ForPlay.graphics().createFont(
+                                         "Helvetica", Font.Style.PLAIN, 64)));
+        root.add(logo, botrow);
+
+        Label bheader = new Label().setText("Start new game:");
+        bheader.addStyles(Style.HALIGN.is(Style.HAlign.CENTER));
+        Button lgame = new Button().setText("Local game");
+        Button ogame = new Button().setText("Online game");
+        Button tgame = new Button().setText("Play-by-email game");
+        buttons.add(bheader, lgame, ogame, tgame);
+
+        Label theader = new Label().setText("Games in-progress:");
+        theader.addStyles(Style.HALIGN.is(Style.HAlign.CENTER));
+        games.add(theader);
+
+        root.setSize(ForPlay.graphics().width(), ForPlay.graphics().height());
+
+        lgame.click.connect(new UnitSlot() {
+            @Override public void onEmit () {
                 startLocalGame(new String[] { "Mahatma Ghandi", "Elvis Presley", "Madonna" });
             }
         });
-    }
-
-    @Override // from Screen
-    public void wasShown () {
-        super.wasShown();
-    }
-
-    @Override // from Screen
-    public void wasHidden () {
-        super.wasHidden();
-    }
-
-    @Override // from Screen
-    public void wasRemoved () {
-        super.wasRemoved();
-        layer.destroy();
     }
 
     protected void startLocalGame (String[] players) {
