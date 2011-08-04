64c1b59f371efcf7285b8fa18a5df7f987b42078
Michael Bayne <mdb@samskivert.com>
2011-08-04 08:42:52 -0700

    Clean up local project handling.
    I tried valiantly to allow local projects to be magically inherited, but due to
    limitations in my understandings of the internals of SBT, I didn't get the
    whole job done. Presently one has to create symlinks for transitive local
    projects in the current project's directory (though they need not be declared
    in the current project's locals). So atlantis inherits forplay from tripleplay,
    but it needs a forplay symlink in its top-level directory for everything to
    work. Similarly (in this case) for react and pythagoras.

--- project/Build.scala
+++ project/Build.scala
@@ -1,27 +1,28 @@
 import sbt._
 import Keys._
 
-object AtlantisBuild extends Build {
-  // allow projects to be symlinked into the current directory for a direct dependency, or fall
-  // back to obtaining the project from maven otherwise
-  val locals = Seq(
-    ("pythagoras", "com.samskivert" % "pythagoras" % "1.1-SNAPSHOT",
-     () => RootProject(file("pythagoras"))),
-    ("forplay", "com.googlecode.forplay" % "core" % "1.0-SNAPSHOT",
-     () => ProjectRef(file("forplay"), "core")),
-    ("tripleplay", "com.threerings" % "tripleplay" % "1.0-SNAPSHOT",
-     () => RootProject(file("tripleplay"))),
-    ("nexus", "com.threerings" % "nexus-gwt-server" % "1.0-SNAPSHOT",
-     () => ProjectRef(file("nexus"), "gwt-server")),
-    ("nexus", "com.threerings" % "nexus-jvm-server" % "1.0-SNAPSHOT",
-     () => ProjectRef(file("nexus"), "jvm-server"))
-  )
-  val (localDeps, localProjs) = locals map {
-    case (id, dep, proj) => if (new java.io.File(id).exists) (None, Some(proj()))
-                            else (Some(dep), None)
-  } unzip
+// allows projects to be symlinked into the current directory for a direct dependency,
+// or fall back to obtaining the project from Maven otherwise
+class Local (locals :(String, String, ModuleID)*) {
+  def addDeps (p :Project) = (locals collect {
+    case (id, subp, dep) if (file(id).exists) => symproj(file(id), subp)
+  }).foldLeft(p) { _ dependsOn _ }
+  def libDeps = locals collect {
+    case (id, subp, dep) if (!file(id).exists) => dep
+  }
+  private def symproj (dir :File, subproj :String = null) =
+    if (subproj == null) RootProject(dir) else ProjectRef(dir, subproj)
+}
 
-  lazy val atlantis = (Project(
+object AtlantisBuild extends Build {
+  val locals = new Local(
+    ("tripleplay", null,    "com.threerings" % "tripleplay" % "1.0-SNAPSHOT"),
+    ("nexus", "gwt-server", "com.threerings" % "nexus-gwt-server" % "1.0-SNAPSHOT"),
+    ("nexus", "jvm-server", "com.threerings" % "nexus-jvm-server" % "1.0-SNAPSHOT")
+  )
+  // TBD: local symlinks are currently also needed for react, pythagoras and forplay
+
+  lazy val atlantis = locals.addDeps(Project(
     "atlantis", file("."), settings = Defaults.defaultSettings ++ Seq(
       organization := "com.threerings",
       version      := "1.0-SNAPSHOT",
@@ -33,7 +34,8 @@ object AtlantisBuild extends Build {
 
       resolvers += "Local Maven Repository" at Path.userHome.asURL + "/.m2/repository",
 
-      libraryDependencies ++= localDeps.flatten ++ Seq(
+      autoScalaLibrary := false, // no scala-library dependency
+      libraryDependencies ++= locals.libDeps ++ Seq(
         // compile dependencies
         "com.google.guava" % "guava" % "r09",
         "com.google.gwt" % "gwt-user" % "2.3.0", // should be exported by forplay-core?
@@ -43,5 +45,5 @@ object AtlantisBuild extends Build {
  	      "com.novocode" % "junit-interface" % "0.7" % "test->default"
       )
     )
-  ) /: localProjs.flatten) { _ dependsOn _ }
+  ))
 }
