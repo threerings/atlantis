f88053d2cf3651df2f682a063132be9204c9334c
Michael Bayne <mdb@samskivert.com>
2011-09-06 16:49:47 -0700

    Various jiggery pokery to get GWT build working from SBT.

--- project/Build.scala
+++ project/Build.scala
@@ -1,5 +1,6 @@
 import sbt._
 import Keys._
+import net.thunderklaus.GwtPlugin._
 
 // allows projects to be symlinked into the current directory for a direct dependency,
 // or fall back to obtaining the project from Maven otherwise
@@ -15,20 +16,24 @@ class Local (locals :(String, String, ModuleID)*) {
 }
 
 object AtlantisBuild extends Build {
+  val playnVersion = "1.0-SNAPSHOT"
+  val nexusVersion = "1.0-SNAPSHOT"
+
   val coreLocals = new Local(
     ("tripleplay", null,  "com.threerings" % "tripleplay" % "1.0-SNAPSHOT"),
-    ("nexus",     "core", "com.threerings" % "nexus-core" % "1.0-SNAPSHOT")
+    ("nexus",     "core", "com.threerings" % "nexus-core" % nexusVersion)
   )
   val htmlLocals = new Local(
-    ("playn", "html",  "com.googlecode.playn" % "playn-html" % "1.0-SNAPSHOT")
+    ("playn", "html",   "com.googlecode.playn" % "playn-html" % playnVersion),
+    ("nexus", "gwt-io", "com.threerings" % "nexus-gwt-io" % nexusVersion)
   )
   val serverLocals = new Local(
-    ("nexus", "gwt-server", "com.threerings" % "nexus-gwt-server" % "1.0-SNAPSHOT"),
-    ("nexus", "jvm-server", "com.threerings" % "nexus-jvm-server" % "1.0-SNAPSHOT")
+    ("nexus", "gwt-server", "com.threerings" % "nexus-gwt-server" % nexusVersion),
+    ("nexus", "jvm-server", "com.threerings" % "nexus-jvm-server" % nexusVersion)
   )
   // TBD: local symlinks are currently also needed for react, pythagoras and playn
 
-  val commonSettings = Seq(
+  val commonSettings = Defaults.defaultSettings ++ Seq(
     organization := "com.threerings",
     version      := "1.0-SNAPSHOT",
     crossPaths   := false,
@@ -39,8 +44,10 @@ object AtlantisBuild extends Build {
   )
 
   lazy val core = coreLocals.addDeps(Project(
-    "core", file("core"), settings = Defaults.defaultSettings ++ commonSettings ++ Seq(
+    "core", file("core"), settings = commonSettings ++ Seq(
       name := "atlantis-core",
+      unmanagedResourceDirectories in Compile <+= baseDirectory / "src/main/java",
+      unmanagedResources in Compile ~= (_.filterNot(_.isDirectory)), // work around SBT bug
       libraryDependencies ++= coreLocals.libDeps ++ Seq(
         // compile dependencies
         "com.google.guava" % "guava" % "r09",
@@ -52,11 +59,14 @@ object AtlantisBuild extends Build {
   ))
 
   lazy val html = htmlLocals.addDeps(Project(
-    "html", file("html"), settings = Defaults.defaultSettings ++ commonSettings ++ Seq(
-      name := "atlantis-html",
+    "html", file("html"), settings = commonSettings ++ gwtSettings ++ Seq(
+      name       := "atlantis-html",
+      gwtVersion := "2.3.0",
       libraryDependencies ++= htmlLocals.libDeps ++ Seq(
         // compile dependencies
-        "com.google.gwt" % "gwt-user" % "2.3.0", // should be exported by playn-core?
+        "com.google.guava" % "guava" % "r09",
+        // TODO: sbt bug? causes above guava to be overridden by below
+        "com.google.guava" % "guava" % "r09" classifier "gwt",
         "allen_sauer" % "gwt-log" % "3.1.4"
       )
     )
---
+++ project/plugins/build.sbt
@@ -0,0 +1,1 @@
+libraryDependencies += "net.thunderklaus" %% "sbt-gwt-plugin" % "1.1-SNAPSHOT"
