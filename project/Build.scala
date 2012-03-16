7f76935f76c800b8ad5c2d70431cfde79d759e2f
Michael Bayne <mdb@samskivert.com>
2012-03-16 11:12:32 -0700

    Wired up Nexus code generation in SBT as well, for future reference.

--- project/Build.scala
+++ project/Build.scala
@@ -2,6 +2,33 @@ import sbt._
 import Keys._
 import samskivert.ProjectBuilder
 
+object Nexus {
+  def NexusGen = sbt.config("nexus") hide
+
+  val nexusGen = TaskKey[Int]("generate", "Generates Nexus streaming code")
+  def nexusGenTask = (streams, javaSource in Compile, dependencyClasspath in Compile,
+                      managedClasspath in NexusGen) map {
+    (s, sourceDir, cp, ncp) => {
+      val copts = Seq("-sourcepath", sourceDir.getPath, "-s", sourceDir.getPath, "-proc:only",
+                      "-classpath", (cp.files ++ ncp.files).map(_.getPath).mkString(":"),
+                      "-processor", "com.threerings.nexus.gencode.Processor",
+                      "-Acom.threerings.nexus.gencode.header=etc/SOURCE_HEADER")
+      val excludes = (sourceDir ** "Streamer_*.java") +++ (sourceDir ** "Factory_*.java")
+      val sources = ((sourceDir ** "*.java") --- excludes).get.map(_.getPath)
+      s.log.debug("Generating Nexus streaming classes")
+      Fork.javac(None, copts ++ sources, StdoutOutput)
+    }
+  }
+
+  def config = Seq[Setting[_]](
+    nexusGen in NexusGen <<= nexusGenTask,
+    ivyConfigurations += NexusGen,
+    managedClasspath in NexusGen <<= (classpathTypes, update) map {
+      (ct, up) => Classpaths.managedJars(NexusGen, ct, up)},
+    libraryDependencies += "com.threerings.nexus" % "nexus-tools" % "1.0-SNAPSHOT" % "nexus"
+  )
+}
+
 object AtlantisBuild extends Build {
   val builder = new ProjectBuilder("pom.xml") {
     override val globalSettings = Seq(
@@ -12,11 +39,11 @@ object AtlantisBuild extends Build {
       autoScalaLibrary := false // no scala-library dependency
     )
     override def projectSettings (name :String) = name match {
-      case "core" => Seq(
+      case "core" => Nexus.config ++ Seq(
         unmanagedResourceDirectories in Compile <+= baseDirectory / "src/main/java",
         libraryDependencies ++= Seq(
           // scala test dependencies
- 	        "com.novocode" % "junit-interface" % "0.7" % "test->default"
+          "com.novocode" % "junit-interface" % "0.7" % "test->default"
         )
       )
       case _ => Nil
